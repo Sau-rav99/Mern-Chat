05b96ae477b796210ac1ee2859a58444d3397b03
Saurav Das <sauravdas1190@gmail.com>
2024-06-23 11:33:36 +0530

    Deployment

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,14 @@
-import Conversations from "./Conversations.jsx";
+import Conversations from "./Conversations";
+import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
-import LogoutButton from "./LogoutButton.jsx";
 
 const Sidebar = () => {
 	return (
 		<div className='border-r border-slate-500 p-4 flex flex-col'>
 			<SearchInput />
 			<div className='divider px-3'></div>
-            <Conversations />
-            <LogoutButton/>
+			<Conversations />
+			<LogoutButton />
 		</div>
 	);
 };
